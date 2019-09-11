1420c69432fc85b69670190b75d4572606741399
Logan L. Owens <owenslo@berea.edu>
2019-09-11 14:02:46 -0400

    We are all Brutus.

--- poem.js
+++ poem.js
@@ -1,10 +1,10 @@
 function nextLine(line) {
 
-	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on github</p>",
-"<p onClick=nextLine(2)>So many pull requests she did snub</p>",
-"<p onClick=nextLine(3)>So I forked the repo</p>",
-"<p onClick=nextLine(4)>Now they all send <em>me</em> changes, d'oh!</p>",
-"<p onClick=nextLine(0)>Said the maintainer, 'Welcome to the club!'</p>");
+	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on gitTub</p>",
+"<p onClick=nextLine(2)>So many requests for spoons they did snub</p>",
+"<p onClick=nextLine(3)>So I forked the backpack</p>",
+"<p onClick=nextLine(4)>Now they all send <em>me</em> quarters, d'oh!</p>",
+"<p onClick=nextLine(0)>Said the maintainer, 'Welcome to the Colosseum!'</p>");
 
 	document.getElementById("line").innerHTML=poem[line];
 }
